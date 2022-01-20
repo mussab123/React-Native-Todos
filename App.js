bd240c7b995929fae7541f6a0dc2be708a1b3f97
mussab123 <mussabassefa8@gmail.com>
2022-01-19 23:14:17 -0600

    modified to include file

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import { LogBox } from 'react-native';
+import AddTodo from "./components/addtodo";
+
 
 export default function App() {
+
+
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1'},
+    { text: 'create an app', key: '2'},
+    { text: 'play on the switch', key: '3'}
+  ]);
+
+  const submitHandler = (text) => {
+    setTodos([...todos, {text: text, key: Math.random().toString()}])
+   }
+
+  const presshandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Header />
+
+      <View style={styles.content}>
+
+
+        <View style={styles.list}>
+
+          <AddTodo submitHandler={submitHandler} />
+
+
+          <FlatList 
+
+
+            data={todos}
+            renderItem={( { item } ) => (
+
+
+              <TodoItem item={item} presshandler={presshandler}/>
+
+            )}  
+          
+          
+          
+          />
+
+        </View>
+      </View>
+
+
+          
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+
+  content: {
+    padding: 40,
+  },
+
+
+  list: {
+    marginTop: 20,
+  }
+
+
+ 
+
 });
+
---
+++ components/todoItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+
+export default function TodoItem({ item, presshandler}){
+
+    return(
+
+        <TouchableOpacity onPress={() => presshandler(item.key)}>
+        <Text style={styles.item}>{item.text}</Text>
+        </TouchableOpacity>
+
+    )
+
+ 
+
+
+}
+
+
+const styles = StyleSheet.create({
+
+    item: {
+        padding: 16,
+        marginTop: 16,
+        borderColor: '#bbb',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderRadius: 10,
+    }
+})
